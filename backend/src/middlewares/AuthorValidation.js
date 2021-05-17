2754310c30f635f2cd3d28f9a1ee9fa306f1762f
Maxson Coelho <maxsoncoelho@gmail.com>
2021-05-17 16:45:33 -0400

    feat: author registration completed

--- backend/src/middlewares/AuthorValidation.js
+++ backend/src/middlewares/AuthorValidation.js
@@ -2,11 +2,11 @@ const AuthorModel = require('../model/AuthorModel');
 
 const AuthorValidation = async (req, res, next) => {
 
-    const { name, age, email, category  } = req.body;
+    const { name, date, email, category  } = req.body;
 
     if(!name)
     return res.status(400).json({ error: 'nome é obrigatório '})
-    else if(!age)
+    else if(!date)
     return res.status(400).json({ error: 'idade é obrigatória' })
     else if(!email)
     return res.status(400).json({ error: 'email é obrigatório' })
--- web/src/views/Authors/index.js
+++ web/src/views/Authors/index.js
@@ -3,11 +3,29 @@ import * as S from './styles';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import CategoryIconsAuthor from '../../utils/categorysAuthor';
+import api from '../../services/api';
 
 
 function Authors() {
-  
-    const [category, setCategory] = useState();
+
+    const [name, setName] = useState();
+    const [date, setDate] =useState();
+    const [email, setEmail] = useState();
+    const [category, setCategory] =useState();
+
+
+    async function save() {
+        await api.post(`/author`, {
+            name,
+            date: `${date}`,
+            email,
+            category
+        }).then(() =>
+            alert('Autor adicionado com sucesso!')
+        )
+        .catch(e => alert(`Falta escolher ou preencher alguma opção${e}`))
+    }
+
 
   return (
     <S.Container>
@@ -27,21 +45,24 @@ function Authors() {
             </S.CategoryIcons>
             <S.Input>
                 <span>Nome</span>
-                <input type="text" placeholder="Insira nome completo"></input>
+                <input type="text" placeholder="Insira nome completo"
+                onChange={e => setName(e.target.value)} value={name}/>
             </S.Input>
             <S.Input>
                 <span>E-mail</span>
-                <input type="email" placeholder="Insira um e-mail"></input>
+                <input type="email" placeholder="Insira um e-mail"
+                onChange={e => setEmail(e.target.value)} value={email}/>
             </S.Input>
             <S.Input>
-                <span>E-mail</span>
-                <input type="date" placeholder="Insira uma data de aniversário"></input>
+                <span>Data de Nascimento</span>
+                <input type="date" placeholder="Insira uma data de aniversário"
+                onChange={e => setDate(e.target.value)} value={date}/>
             </S.Input>
             <S.Delete>
                 <button type="button" alt="deletar autor">Excluir Autor</button>
             </S.Delete>
             <S.Save>
-                <button type="button" >Salvar</button>
+                <button type="button" onClick={save} >Salvar</button>
             </S.Save>
         </S.Form>
 
