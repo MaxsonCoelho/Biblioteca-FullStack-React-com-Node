a32e4f54636904f76435e304427b2272e8f0761f
Maxson Coelho <maxsoncoelho@gmail.com>
2021-05-17 16:02:40 -0400

    feat: layout page author ok

--- web/src/views/Authors/index.js
+++ web/src/views/Authors/index.js
@@ -7,6 +7,7 @@ import CategoryIconsAuthor from '../../utils/categorysAuthor';
 
 function Authors() {
   
+    const [category, setCategory] = useState();
 
   return (
     <S.Container>
@@ -15,11 +16,33 @@ function Authors() {
         <S.Form>
             <S.CategoryIcons>
                 {
-                    CategoryIconsAuthor.map(icon => (
-                        <img src={icon} alt="Tipo da Tarefa" />
+                    CategoryIconsAuthor.map((icon, index) => (
+                        index > 0 &&
+                        <button type="button" onClick={() => setCategory(index)}>
+                            <img src={icon} alt="Categoria do autor" 
+                            className={category && category != index && 'inative'} />
+                        </button>
                     ))
                 }
             </S.CategoryIcons>
+            <S.Input>
+                <span>Nome</span>
+                <input type="text" placeholder="Insira nome completo"></input>
+            </S.Input>
+            <S.Input>
+                <span>E-mail</span>
+                <input type="email" placeholder="Insira um e-mail"></input>
+            </S.Input>
+            <S.Input>
+                <span>E-mail</span>
+                <input type="date" placeholder="Insira uma data de aniversário"></input>
+            </S.Input>
+            <S.Delete>
+                <button type="button" alt="deletar autor">Excluir Autor</button>
+            </S.Delete>
+            <S.Save>
+                <button type="button" >Salvar</button>
+            </S.Save>
         </S.Form>
 
         <Footer />
--- web/src/views/Authors/styles.js
+++ web/src/views/Authors/styles.js
@@ -6,20 +6,103 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-bottom: 100px;
 `
 
 
 export const Form = styled.div`
     display: flex;
     width: 50%;
+    flex-direction: column;
 `
 
 export const CategoryIcons = styled.div`
     display: flex;
-    width: 50px;
+    width: 100%;
+    justify-content: center;
+    
+    .inative{
+        opacity: 0.5;
+    }
+    
+
+    button{
+        border: none;
+        background: none;
+
+    }
 
     img{
         height: 50px;
         width: 50px;
+        margin: 10px;
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.5;
+        }
     }
+`
+
+export const Input = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0;
+
+    span{
+        color: #707070;
+        margin-bottom: 5px;
+    }
+
+    input{
+        font-size: 16px;
+        padding: 15px;
+        border: none;
+        border-bottom: 1px solid #610B21;
+    }
+
+`
+
+export const Delete = styled.div`
+    display: flex;
+    justify-content: flex-end;
+
+
+    button{
+        font-weight: bold;
+        color: #610B21;
+        border: none;
+        background: none;
+        font-size: 17px;
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.5;
+        }
+    }
+
+`
+
+export const Save = styled.div`
+    width: 100%;
+    margin-top: 20px;
+
+    button{
+        width: 100%;
+        background: #610B21;
+        border: none;
+        font-size: 20px;
+        color: #FFF;
+        font-weight: bold;
+        padding: 20px;
+        border-radius: 30px;
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.7;
+        }
+    }
+
+
 `
